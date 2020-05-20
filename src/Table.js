dfdf654ec984e85fc321afbc9144803d482d6230
BitterDone <bitter.daniel@gmail.com>
2020-05-19 22:03:07 -0500

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -1,35 +1,33 @@
-//import './css/jquery.dataTables.css'
-import 'datatables.net-dt/css/jquery.datatables.css'
+import '../../node_modules/datatables.net-dt/css/jquery.dataTables.css'
 import React, {Component} from 'react'
-//import App from "./App";
 
-const $ = require('jquery')
-$.DataTable = require('datatables.net')
-require( 'datatables.net-bs' )( window, $ );
+var $       = require( 'jquery' );
+var dt      = require( 'datatables.net' );
 
 export class Table extends Component {
     componentDidMount() {
-        console.log(this.el);
         this.$el = $(this.el)
         this.$el.DataTable({
             data: this.props.data,
-            columns: [
-                { title: "Name" },
-                { title: "Position" },
-                { title: "Office" },
-                { title: "Extn." },
-                { title: "Start date" },
-                { title: "Salary" }
-            ]
+            columns: this.props.columns,
+            lengthMenu: [[-1], ["All"]],
+            scrollY:        '75vh',
+            scrollCollapse: true,
         })
     }
 
-    componentWillUnmount() {
+    updateTableData(data) {
+        this.$el.dataTable().api().clear();
+        this.$el.dataTable().api().rows.add(data);
+        this.$el.dataTable().api().draw();
     }
 
     render() {
+        if(this.$el) {
+            this.updateTableData(this.props.data)
+        }
         return (
-            <div>
+            <div style={this.props.style}>
                 <table className="display" width="100%" ref = {el => this.el = el }></table>
 
             </div>
